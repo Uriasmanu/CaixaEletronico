1b4fdc1fe2c516965de739d90a6b66cbccc1d964
Manoela Urias <maanoelaurias@gmail.com>
2025-05-29 22:27:13 -0300

    cria modelo

--- CaixaEletronico/Models/Conta.cs
+++ CaixaEletronico/Models/Conta.cs
@@ -11,6 +11,8 @@ namespace CaixaEletronico.Models
         public Guid Id { get; set; }
         public int NumeroConta { get; set; }
         public string NameTitular { get; set; }
-
+        public double Saldo { get; set; }
+        public int Senha { get; set; }
+        
     }
 }
--- CaixaEletronico/Program.cs
+++ CaixaEletronico/Program.cs
@@ -1,5 +1,13 @@
 ﻿
+
+using CaixaEletronico.Models;
+
+var conta = new Conta();
+
 Console.WriteLine("Bem-vindo ao Caixa Eletrônico");
 Console.WriteLine("\nDigite o número da conta: 1234");
+conta.NumeroConta = int.Parse(Console.ReadLine());
 Console.WriteLine("\nDigite a senha: ****");
+conta.Senha = int.Parse(Console.ReadLine());
+
 
