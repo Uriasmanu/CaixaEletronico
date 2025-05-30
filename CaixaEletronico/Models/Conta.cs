b5974a60f9eb5883ffa420dd04a5d819d2a25620
Manoela Urias <maanoelaurias@gmail.com>
2025-05-29 22:11:01 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaEletronico/Models/Conta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico.Models
+{
+    internal class Conta
+    {
+        public Guid Id { get; set; }
+        public int NumeroConta { get; set; }
+        public string NameTitular { get; set; }
+
+    }
+}
---
+++ CaixaEletronico/Program.cs
@@ -0,0 +1,5 @@
+﻿
+Console.WriteLine("Bem-vindo ao Caixa Eletrônico");
+Console.WriteLine("\nDigite o número da conta: 1234");
+Console.WriteLine("\nDigite a senha: ****");
+
