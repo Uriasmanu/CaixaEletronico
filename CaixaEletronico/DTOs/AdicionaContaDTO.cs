82fa9b64759c6623ae8f0a4c7018e891eab012d9
Manoela Urias <manoela.urias@treetech.com.br>
2025-06-20 17:32:55 -0300

    adiciona classe

---
+++ CaixaEletronico/DTOs/AdicionaContaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico.Models
+{
+    internal class AdicionaContaDTO
+    {
+        public string NameTitular { get; set; }
+        public int Senha { get; set; }
+        
+    }
+}
--- CaixaEletronico/Program.cs
+++ CaixaEletronico/Program.cs
@@ -1,13 +1,45 @@
 ﻿
 
 using CaixaEletronico.Models;
+using System.Linq.Expressions;
 
-var conta = new Conta();
+var conta = new AdicionaContaDTO();
+int opcao;
 
 Console.WriteLine("Bem-vindo ao Caixa Eletrônico");
-Console.WriteLine("\nDigite o número da conta: 1234");
-conta.NumeroConta = int.Parse(Console.ReadLine());
-Console.WriteLine("\nDigite a senha: ****");
-conta.Senha = int.Parse(Console.ReadLine());
+Console.WriteLine("\nSelecione uma opção para prosseguir");
+Console.WriteLine("\n*******************************");
+Console.WriteLine("\n1 - Para se Cadastrar");
+Console.WriteLine("\n2 - Para Consultar Saldo");
+Console.WriteLine("\n3 - Para Sacar");
+Console.WriteLine("\n1 - Para Tranferir");
+Console.WriteLine("\n*******************************");
+opcao = int.Parse(Console.ReadLine());
+
+
+
+switch (opcao)
+{
+    case 1:
+        Console.WriteLine($"Você escolheu a opção {opcao} e será direcionado à tela de Cadastro");
+        break;
+
+    case 2:
+        Console.WriteLine("Você escolheu consultar saldo.");
+        break;
+
+    case 3:
+        Console.WriteLine("Você escolheu sacar dinheiro.");
+        break;
+
+    case 4:
+        Console.WriteLine("Você escolheu transferir.");
+        break;
+
+    default:
+        Console.WriteLine("Opção inválida, escolha novamente.");
+        break;
+}
+
 
 
