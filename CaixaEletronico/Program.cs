781ba2aca540ee81820aa389f08c49d508c7b1fb
Manoela Urias <manoela.urias@treetech.com.br>
2025-06-20 22:40:48 -0300

    adiciona logica

--- CaixaEletronico/Program.cs
+++ CaixaEletronico/Program.cs
@@ -21,19 +21,19 @@ opcao = int.Parse(Console.ReadLine());
 switch (opcao)
 {
     case 1:
-        Console.WriteLine($"Você escolheu a opção {opcao} e será direcionado à tela de Cadastro");
+        Console.WriteLine($"\nVocê escolheu a opção {opcao} e será direcionado à tela de Cadastro");
         break;
 
     case 2:
-        Console.WriteLine("Você escolheu consultar saldo.");
+        Console.WriteLine($"\nVocê escolheu a opção {opcao} e seu saldo é: ");
         break;
 
     case 3:
-        Console.WriteLine("Você escolheu sacar dinheiro.");
+        Console.WriteLine($"\nVocê escolheu a opção {opcao} e será direcionado à tela de Saques");
         break;
 
     case 4:
-        Console.WriteLine("Você escolheu transferir.");
+        Console.WriteLine($"\nVocê escolheu a opção {opcao} e será direcionado para a tela de transferencia.");
         break;
 
     default:
